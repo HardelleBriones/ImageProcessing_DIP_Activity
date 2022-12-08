fe747ec8557693a22e9b459795f4ab3d40ccae78
Hardelle Briones <111712687+HardelleBriones@users.noreply.github.com>
2022-12-08 21:57:47 +0800

    Add GreenScreen form feature

---
+++ ImageProcessing_Activity/GreenScreen.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ImageProcessing_DIP2_Activity
+{
+    public partial class GreenScreen : Form
+    {
+        Bitmap loaded, backgroundImage, result;
+        public GreenScreen()
+        {
+            InitializeComponent();
+            openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.RestoreDirectory = true;
+            openFileDialog1.InitialDirectory = "C:\\";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.Filter = "bmp Files (*.bmp)|*.bmp|jpg Files (*.jpg)|*.jpg|gif Files (*.gif)|*.gif|png Files (*.png)|*.png ";
+
+            saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.RestoreDirectory = true;
+            saveFileDialog1.InitialDirectory = "C:\\";
+            saveFileDialog1.FilterIndex = 1;
+            saveFileDialog1.Filter = "jpg Files (*.jpg)|*.jpg|gif Files (*.gif)|*.gif|png Files (*.png)|*.png |bmp Files (*.bmp)|*.bmp";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void btnLoadImage_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.OK == openFileDialog1.ShowDialog())
+            {
+                loaded = new Bitmap(openFileDialog1.FileName);
+                pictureBox1.Image = loaded;
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        }
+
+        private void btnLoadImageBackGroudn_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.OK == openFileDialog1.ShowDialog())
+            {
+                backgroundImage = new Bitmap(openFileDialog1.FileName);
+                pictureBox2.Image = backgroundImage;
+                pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.OK == saveFileDialog1.ShowDialog())
+            {
+                Image img = (Image)result;
+                img.Save(saveFileDialog1.FileName);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            result = new Bitmap(loaded.Width, loaded.Height);
+            
+            Color green = Color.FromArgb(0, 0, 255);
+            int greygreen = (green.R + green.G + green.B) / 3;
+            int threshold = 5;
+            for (int x = 0; x < loaded.Width; x++)
+                for (int y = 0; y < loaded.Height; y++)
+                {
+                    Color pixel = loaded.GetPixel(x, y);
+                    Color backpixel = backgroundImage.GetPixel(x, y);
+                    int grey = (pixel.R + pixel.G + pixel.B) / 3;
+                    int subtractvalue = Math.Abs(grey - greygreen);
+                    if (subtractvalue > threshold)
+                        result.SetPixel(x, y, pixel);
+                    else
+                        result.SetPixel(x, y, backpixel);
+                        
+                }
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.Image = result;
+            
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.OK == openFileDialog1.ShowDialog())
+            {
+                loaded = new Bitmap(openFileDialog1.FileName);
+                pictureBox1.Image = loaded;
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        }
+    }
+}
