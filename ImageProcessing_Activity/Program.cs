86b144665162952c6ec3660390bfbe41206ce6cb
Hardelle Briones <111712687+HardelleBriones@users.noreply.github.com>
2022-12-08 21:58:17 +0800

    Rename the Form1

--- ImageProcessing_Activity/Program.cs
+++ ImageProcessing_Activity/Program.cs
@@ -15,7 +15,7 @@ namespace ImageProcessing_Activity
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new BasicImageProcessing());
         }
     }
 }
